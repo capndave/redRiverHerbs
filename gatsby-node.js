3a4c5bd0c839846ec2b58cdd10b67897d4a8b67f
capndave <capndavet@gmail.com>
2020-03-28 13:39:29 -0500

    Deleted redirect in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,17 +2,6 @@ const path = require('path')
 
 exports.createPages = async ({ graphql, actions }) => {
 
-    // /* Redirect from root to products page immediately */
-
-    // const { createRedirect } = actions
-
-    // createRedirect({
-    //   fromPath: `/`,
-    //   toPath: `/products`,
-    //   redirectInBrowser: true,
-    //   isPermanent: true,
-    // })
-
     /* Generate (detail) pages for each product */
 
     const response = await graphql(`
